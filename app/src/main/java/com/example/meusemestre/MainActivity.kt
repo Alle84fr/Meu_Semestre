6301d54e7b858e962e4c2debdc1426b09811cdc8
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-05-22 16:13:41 -0300

    nova branch ao trabalho, para testes

--- app/src/main/java/com/example/meusemestre/MainActivity.kt
+++ app/src/main/java/com/example/meusemestre/MainActivity.kt
@@ -5,8 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-// teste...
-// teste02
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
