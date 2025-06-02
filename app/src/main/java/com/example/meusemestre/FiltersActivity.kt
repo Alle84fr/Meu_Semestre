16b57d63286bd2757703a03f9b32be36f3c0b1aa
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-06-02 13:10:10 -0300

    add erros cadastro plano resultado

---
+++ app/src/main/java/com/example/meusemestre/FiltersActivity.kt
@@ -0,0 +1,13 @@
+package com.example.meusemestre
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FiltersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
