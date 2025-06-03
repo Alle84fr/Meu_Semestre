6de1cb50473053a8d17e3cc941852d49076d1bb1
Gabriel <gbr.vianas@gmail.com>
2025-06-03 17:53:42 -0300

    corrigindo ids

--- app/src/main/java/com/example/meusemestre/PlanoActivity.kt
+++ app/src/main/java/com/example/meusemestre/PlanoActivity.kt
@@ -11,7 +11,7 @@ class PlanoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_plano)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btn)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
